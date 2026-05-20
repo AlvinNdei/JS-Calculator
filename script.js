9b3e16603c1b5a0085850085fe8f9b5d89dfc7e2
AlvinNdei <alvinmuriuki714@gmail.com>
2026-05-20 23:47:44 +0300

    Syntax

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let button=document.getElementById('mybutton');
 button.addEventListener("click",function(){
-    alert('Your Info was submitted');
-    console.log('User clicked the submit button');
+    alert('Your info was submitted');
+});
+let heading=document.querySelector("h1");
+heading.addEventListener('mouseover',function(){
+    heading.style.color="red";
+});
+heading.addEventListener('mouseout',function(){
+    heading.style.color="black";
+});
+let btt=document.querySelector("button");
+btt.addEventListener('mouseover',function(){
+    btt.style.color="blue";
+});
+btt.addEventListener('mouseout',function(){
+    btt.style.color="black";
+});
+document.addEventListener("keydown",function(event){
+    console.log('You pressed the ${event.key} key');
+    if (event.key==="Enter"){
+        console.log("Enter key pressed");
+    }
 });
